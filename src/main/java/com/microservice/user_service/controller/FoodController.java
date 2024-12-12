98540d0e95fe0ee0e5eaa7628d9ebe164c2d66d1
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-12-12 15:55:41 -0800

    made food model

--- src/main/java/com/microservice/user_service/controller/FoodController.java
+++ src/main/java/com/microservice/user_service/controller/FoodController.java
@@ -5,11 +5,12 @@ import java.io.IOException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 public class FoodController {
-
+    @Hidden
     @RequestMapping(value="/")
     public void redirect(HttpServletResponse response) throws IOException{
         response.sendRedirect("/swagger-ui.html");
---
+++ src/main/java/com/microservice/user_service/model/Food.java
@@ -0,0 +1,22 @@
+package com.microservice.user_service.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "food") 
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor 
+public class Food {
+
+    @Id
+    private String id; 
+
+    private String name;
+    private int protein;
+    private int carb;
+    private int fat;
+}
