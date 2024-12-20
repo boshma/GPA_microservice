30ccd8fa0661ab634e9c2915448dc06edc49d3cc
Bogdan Shmat <bogdan.shmat@wsu.edu>
2024-12-20 09:13:13 -0800

    remade food model to match api specification

--- src/main/java/com/microservice/user_service/model/Food.java
+++ src/main/java/com/microservice/user_service/model/Food.java
@@ -2,21 +2,38 @@ package com.microservice.user_service.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Document(collection = "FoodandExercises") 
-@Data 
-@NoArgsConstructor 
-@AllArgsConstructor 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Document(collection = "FoodandExercises")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Food {
 
     @Id
     private String id; 
 
-    private String name;
-    private int protein;
-    private int carb;
-    private int fat;
+    private String name; 
+
+    private double protein; 
+    private double carb;  
+    private double fat;   
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate date; 
+
+    private String userId; 
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime createdAt; 
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime updatedAt;
 }
