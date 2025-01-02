50e4801e00aed7e19684e0228d321f2ab8931b17
Bogdan Shmat <bogdan.shmat@wsu.edu>
2025-01-01 18:09:30 -0800

    add back in groupedOpenAPI

--- src/main/java/com/microservice/user_service/config/OpenApiConfig.java
+++ src/main/java/com/microservice/user_service/config/OpenApiConfig.java
@@ -1,15 +1,23 @@
 package com.microservice.user_service.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public") 
+                .pathsToMatch("/**") 
+                .build();
+    }
+
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -17,8 +25,8 @@ public class OpenApiConfig {
                 .components(new Components()
                         .addSecuritySchemes("apiKey", new SecurityScheme()
                                 .name("X-API-KEY")
-                                .type(SecurityScheme.Type.APIKEY)
-                                .in(SecurityScheme.In.HEADER)))
-                .addSecurityItem(new SecurityRequirement().addList("apiKey"));
+                                .type(SecurityScheme.Type.APIKEY) 
+                                .in(SecurityScheme.In.HEADER))) 
+                .addSecurityItem(new SecurityRequirement().addList("apiKey")); 
     }
 }
