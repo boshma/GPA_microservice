c4585f3a0e41821687a3a56caf7395ac421c750f
Bogdan Shmat <bogdan.shmat@wsu.edu>
2025-01-12 09:22:54 -0800

    add back in redirect to openapi

--- src/main/java/com/microservice/user_service/controller/FoodController.java
+++ src/main/java/com/microservice/user_service/controller/FoodController.java
@@ -1,5 +1,6 @@
 package com.microservice.user_service.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.*;
 import com.microservice.user_service.model.Food;
 import com.microservice.user_service.service.FoodService;
 
+import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/food")
@@ -29,6 +32,12 @@ public class FoodController {
 
     @Autowired
     private FoodService foodService;
+
+    @Hidden
+    @RequestMapping(value="/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
 
     private String getCurrentUserId() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
