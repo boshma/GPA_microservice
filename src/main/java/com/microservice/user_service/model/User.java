8a7a7b55e156167a5bb9b3a0af74ab62e05d4cd1
Bogdan Shmat <bogdan.shmat@wsu.edu>
2025-01-20 20:06:51 -0800

    user validation annotations

--- src/main/java/com/microservice/user_service/model/User.java
+++ src/main/java/com/microservice/user_service/model/User.java
@@ -4,19 +4,27 @@ import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDateTime;
 
 @Document(collection = "Users")
 @Data
 public class User {
-
     @Id
-    private String id; 
+    private String id;
 
-    private String username; 
-    private String email;   
-    private String password; 
+    @NotBlank(message = "Username is required")
+    private String username;
 
-    private LocalDateTime createdAt; 
-    private LocalDateTime updatedAt; 
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
